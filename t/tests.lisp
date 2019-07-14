21e74fd84e09288fc357f29319cd72ba9fe316dc
Neil Lindquist <NeilLindquist5@gmail.com>
2019-07-14 00:35:34 -0400

    Add tests for Gitlab CI

--- t/tests.lisp
+++ t/tests.lisp
@@ -31,6 +31,14 @@
   (is (eq (not (null (uiop:getenvp "APPVEYOR_PULL_REQUEST_NUMBER")))
           (is-pr))))
 
+(test :gitlab-ci-tests
+  (is-true (member :gitlab-ci *features*))
+  (is-false (member :circleci *features*))
+  (is (eq :gitlab-ci (platform)))
+  (is (string= (uiop:getenv "CI_COMMIT_REF_NAME") (branch)))
+  (is (eq (not (null (uiop:getenvp "CI_MERGE_REQUEST_ID")))
+          (is-pr))))
+
 (test :user-tests
   (is-true (member :not-ci *features*))
   (is-false (member :ci *features*))
