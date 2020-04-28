7473fa2eac8c9de380e6dbbe0eee5add18961291
Bart Botta <00003b@gmail.com>
2020-04-27 22:25:16 -0500

    testing

--- t/test-system-compile-warn.lisp
+++ t/test-system-compile-warn.lisp
@@ -2,6 +2,8 @@
   (:use :cl))
 (in-package :ci-utils-test-system-compile-warn)
 
+(format t "loading~%")
 (eval-when (:compile-toplevel)
+  (format t "compiling~%")
   (warn "compilation warning!"))
 
