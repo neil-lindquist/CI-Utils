ca791cb5c6851aa672c195ad5cfdaab69ebc82fc
Neil Lindquist <neillindquist5@gmail.com>
2020-09-13 22:37:45 -0400

    Adjust tests for pull request detection to just look at truthyness

--- t/tests.lisp
+++ t/tests.lisp
@@ -11,56 +11,56 @@
   (is-false (member :circleci *features*))
   (is (eq :travis-ci (platform)))
   (is (string= (uiop:getenv "TRAVIS_BRANCH") (branch)))
-  (is (eq (uiop:getenvp "TRAVIS_PULL_REQUEST_BRANCH")
-          (pull-request-p))))
+  (is (eq (null (uiop:getenvp "TRAVIS_PULL_REQUEST_BRANCH"))
+          (null (pull-request-p)))))
 
 (test :circleci-tests
   (is-true (member :circleci *features*))
   (is-false (member :travis-ci *features*))
   (is (eq :circleci (platform)))
   (is (string= (uiop:getenv "CIRCLE_BRANCH") (branch)))
-  (is (eq (uiop:getenvp "CIRCLE_PULL_REQUEST")
-          (pull-request-p))))
+  (is (eq (null (uiop:getenvp "CIRCLE_PULL_REQUEST"))
+          (null (pull-request-p)))))
 
 (test :appveyor-tests
   (is-true (member :appveyor *features*))
   (is-false (member :circleci *features*))
   (is (eq :appveyor (platform)))
   (is (string= (uiop:getenv "APPVEYOR_REPO_BRANCH") (branch)))
-  (is (eq (uiop:getenvp "APPVEYOR_PULL_REQUEST_NUMBER")
-          (pull-request-p))))
+  (is (eq (null (uiop:getenvp "APPVEYOR_PULL_REQUEST_NUMBER"))
+          (null (pull-request-p)))))
 
 (test :gitlab-ci-tests
   (is-true (member :gitlab-ci *features*))
   (is-false (member :circleci *features*))
   (is (eq :gitlab-ci (platform)))
   (is (string= (uiop:getenv "CI_COMMIT_REF_NAME") (branch)))
-  (is (eq (uiop:getenvp "CI_MERGE_REQUEST_ID")
-          (pull-request-p))))
+  (is (eq (null (uiop:getenvp "CI_MERGE_REQUEST_ID"))
+          (null (pull-request-p)))))
 
 (test :bitbucket-pipelines-tests
   (is-true (member :bitbucket-pipelines *features*))
   (is-false (member :circleci *features*))
   (is (eq :bitbucket-pipelines (platform)))
   (is (string= (uiop:getenv "BITBUCKET_BRANCH") (branch)))
-  (is (eq (uiop:getenvp "BITBUCKET_PR_ID")
-          (pull-request-p))))
+  (is (eq (null (uiop:getenvp "BITBUCKET_PR_ID"))
+          (null (pull-request-p)))))
 
 (test :azure-pipelines-tests
   (is-true (member :azure-pipelines *features*))
   (is-false (member :circleci *features*))
   (is (eq :azure-pipelines (platform)))
   (is (string= (uiop:getenv "BUILD_SOURCEBRANCHNAME") (branch)))
-  (is (eq (uiop:getenvp "SYSTEM_PULLREQUEST_PULLREQUESTID")
-          (pull-request-p))))
+  (is (eq (null (uiop:getenvp "SYSTEM_PULLREQUEST_PULLREQUESTID"))
+          (null (pull-request-p)))))
 
 (test :github-actions-tests
   (is-true (member :github-actions *features*))
   (is-false (member :circleci *features*))
   (is (eq :github-actions  (platform)))
   (is (string= (subseq (uiop:getenv "GITHUB_REF") 11) (branch)))
-  (is (eq (string= "pull_request" (uiop:getenvp "GITHUB_EVENT_NAME"))
-          (pull-request-p))))
+  (is (eq (null (string= "pull_request" (uiop:getenvp "GITHUB_EVENT_NAME")))
+          (null (pull-request-p)))))
 
 (test :base-tests
   (is-true (member :ci *features*))
